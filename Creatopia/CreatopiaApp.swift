de8061a9c6471bd38053847d6fed0131305cb569
shahad <shada-123@hotmail.com>
2026-02-04 12:57:30 +0300

    Initial Commit

---
+++ Creatopia/CreatopiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatopiaApp.swift
+//  Creatopia
+//
+//  Created by shahad khaled on 16/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct CreatopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
