6b941dd8e7606650f0b560bcbbdb0bfad415b7ce
Sarah <Sarahalsarah77@gmail.com>
2026-02-08 21:13:09 +0300

    canceled model and viewmodel

--- Creatopia/Model/DIYtimerModel.swift
+++ Creatopia/Model/DIYtimerModel.swift
@@ -5,14 +5,14 @@
 //  Created by Sarah Khalid Almalki on 20/08/1447 AH.
 //
 
-// Timer states (enum)
-enum TimerState {
-    case running
-    case paused
-    case finished
-}
-
-// Optional constants
-struct TimerConstants {
-    static let defaultDuration = 120 // seconds
-}
+//// Timer states (enum)
+//enum TimerState {
+//    case running
+//    case paused
+//    case finished
+//}
+//
+//// Optional constants
+//struct TimerConstants {
+//    static let defaultDuration = 120 // seconds
+//}
--- Creatopia/ViewModel/DIYtimerViewModel.swift
+++ Creatopia/ViewModel/DIYtimerViewModel.swift
@@ -5,50 +5,50 @@
 //  Created by Sarah Khalid Almalki on 20/08/1447 AH.
 //
 
-import SwiftUI
-import Combine
-
-class DIYtimerViewModel: ObservableObject {
-    @Published var timeRemaining: Int
-    @Published var timerState: TimerState
-    @Published var showConfetti: Bool = false
-
-    private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    private var cancellables = Set<AnyCancellable>()
-
-    init(duration: Int = TimerConstants.defaultDuration) {
-        self.timeRemaining = duration
-        self.timerState = .running
-
-        timer
-            .sink { [weak self] _ in
-                self?.tick()
-            }
-            .store(in: &cancellables)
-    }
-
-    private func tick() {
-        guard timerState == .running else { return }
-
-        if timeRemaining > 0 {
-            timeRemaining -= 1
-        } else {
-            timerState = .finished
-            showConfetti = true
-        }
-    }
-
-    func restart() {
-        timeRemaining = TimerConstants.defaultDuration
-        timerState = .running
-        showConfetti = false
-    }
-
-    func pause() {
-        if timerState == .running {
-            timerState = .paused
-        } else if timerState == .paused {
-            timerState = .running
-        }
-    }
-}
+//import SwiftUI
+//import Combine
+//
+//class DIYtimerViewModel: ObservableObject {
+//    @Published var timeRemaining: Int
+//    @Published var timerState: TimerState
+//    @Published var showConfetti: Bool = false
+//
+//    private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+//    private var cancellables = Set<AnyCancellable>()
+//
+//    init(duration: Int = TimerConstants.defaultDuration) {
+//        self.timeRemaining = duration
+//        self.timerState = .running
+//
+//        timer
+//            .sink { [weak self] _ in
+//                self?.tick()
+//            }
+//            .store(in: &cancellables)
+//    }
+//
+//    private func tick() {
+//        guard timerState == .running else { return }
+//
+//        if timeRemaining > 0 {
+//            timeRemaining -= 1
+//        } else {
+//            timerState = .finished
+//            showConfetti = true
+//        }
+//    }
+//
+//    func restart() {
+//        timeRemaining = TimerConstants.defaultDuration
+//        timerState = .running
+//        showConfetti = false
+//    }
+//
+//    func pause() {
+//        if timerState == .running {
+//            timerState = .paused
+//        } else if timerState == .paused {
+//            timerState = .running
+//        }
+//    }
+//}
