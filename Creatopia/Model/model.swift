5a4d65f45b0d79e244a58befab52f8c53355783a
shahad <shada-123@hotmail.com>
2026-02-08 10:39:11 +0300

    model/viewmodel

---
+++ Creatopia/Model/model.swift
@@ -0,0 +1,7 @@
+//
+//  model.swift
+//  Creatopia
+//
+//  Created by shahad khaled on 20/08/1447 AH.
+//
+
---
+++ Creatopia/ViewModel/viewmodel.swift
@@ -0,0 +1,7 @@
+//
+//  viewmodel.swift
+//  Creatopia
+//
+//  Created by shahad khaled on 20/08/1447 AH.
+//
+
