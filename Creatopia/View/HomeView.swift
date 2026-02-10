78115ad371b64084d65c817c4539a64b8a24816b
maram <ma.ram.5.sky5@gmail.com>
2026-02-10 11:56:24 +0300

    h

--- Creatopia/View/HomeView.swift
+++ Creatopia/View/HomeView.swift
@@ -16,60 +16,74 @@ struct HomeView: View {
     @State private var moveBox = false
 
     var body: some View {
-        ZStack {
-
-            Image("home")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
-
-            // Art
-            Button {
-                print("Art tapped")
-            } label: {
-                Image("Art")
+        NavigationStack{
+            ZStack {
+                
+                Image("home")
                     .resizable()
-                    .frame(width: 199, height: 247)
-                    .rotationEffect(.degrees(moveArt ? 3 : -3))
-                    .animation(.easeInOut(duration: 0.5).repeatForever(), value: moveArt)
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                
+                // Art
+                Button {
+                    print("Art tapped")
+                } label: {
+                    Image("Art")
+                        .resizable()
+                        .frame(width: 199, height: 247)
+                        .rotationEffect(.degrees(moveArt ? 3 : -3))
+                        .animation(.easeInOut(duration: 0.5).repeatForever(), value: moveArt)
+                }
+                .position(x: 115, y: 189)
+                .onAppear { moveArt = true }
+                
+                // shelf
+                Button {
+                    print("Shelf tapped")
+                } label: {
+                    Image("shelf")
+                        .resizable()
+                        .frame(width: 270, height: 210)
+                        .rotationEffect(.degrees(moveShelf ? 3 : -3))
+                        .animation(.easeInOut(duration: 0.5).repeatForever(), value: moveShelf)
+                }
+                .position(x: 1017, y: 211)
+                .onAppear { moveShelf = true }
+                
+                // table
+                NavigationLink {
+                    // Destination when tapping the table
+                    ShakeDetector()
+                        .onAppear {
+                            // Restart the wobble animation on navigation if desired
+                            moveTable = false
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+                                moveTable = true
+                            }
+                        }
+                } label: {
+                    Image("table")
+                        .resizable()
+                        .frame(width: 690, height: 457)
+                        .rotationEffect(.degrees(moveTable ? 2 : -2))
+                        .animation(.easeInOut(duration: 0.5).repeatForever(), value: moveTable)
+                }
+                .position(x: 702, y: 656)
+                .onAppear { moveTable = true }
+                
+                // box
+                Button {
+                    print("Box tapped")
+                } label: {
+                    Image("box")
+                        .resizable()
+                        .frame(width: 290, height: 252)
+                        .rotationEffect(.degrees(moveBox ? 3 : -3))
+                        .animation(.easeInOut(duration: 0.5).repeatForever(), value: moveBox)
+                }
+                .position(x: 180, y: 795)
+                .onAppear { moveBox = true }
             }
-            .position(x: 115, y: 189)
-            .onAppear { moveArt = true }
-
-            // shelf
-            Button {
-                print("Shelf tapped")
-            } label: {
-                Image("shelf")
-                    .resizable()
-                    .frame(width: 270, height: 210)
-                    .rotationEffect(.degrees(moveShelf ? 3 : -3))                    .animation(.easeInOut(duration: 0.5).repeatForever(), value: moveShelf)
-            }
-            .position(x: 1017, y: 211)
-            .onAppear { moveShelf = true }
-
-            // table
-            Button {
-            } label: {
-                Image("table")
-                    .resizable()
-                    .frame(width: 690, height: 457)
-                    .rotationEffect(.degrees(moveTable ? 2 : -2))                    .animation(.easeInOut(duration: 0.5).repeatForever(), value: moveTable)
-            }
-            .position(x: 702, y: 656)
-            .onAppear { moveTable = true }
-
-            // box
-            Button {
-                print("Box tapped")
-            } label: {
-                Image("box")
-                    .resizable()
-                    .frame(width: 290, height: 252)
-                    .rotationEffect(.degrees(moveBox ? 3 : -3))                    .animation(.easeInOut(duration: 0.5).repeatForever(), value: moveBox)
-            }
-            .position(x: 180, y: 795)
-            .onAppear { moveBox = true }
         }
     }
 }
