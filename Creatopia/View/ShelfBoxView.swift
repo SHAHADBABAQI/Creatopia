38d59d1a0fe756f84c61d3769650c9608f91673d
Rama <ramabader891919@gmail.com>
2026-02-10 11:50:12 +0300

    ui almost done

--- Creatopia/View/ShelfBoxView.swift
+++ Creatopia/View/ShelfBoxView.swift
@@ -1,86 +1,94 @@
 import SwiftUI
 
 struct ShelfBoxView: View {
-    
-    @StateObject private var viewModel = ShelfBoxViewModel()
-    
     var body: some View {
-        GeometryReader { geo in
-            HStack(spacing: 0) {
-                
-                ZStack {
-                    
-                    Color(hex: "CCB490")                        .frame(
-                            width: geo.size.width / 2,
-                            height: geo.size.height
-                        )
-                    
-                    // Box في النص
-                    Image("inbox")
-                        .resizable()
-                        .frame(width: 673, height: 529)
-                        .position(
-                            x: (geo.size.width / 2) / 2,
-                            y: geo.size.height / 2
-                        )
-                }
-                .frame(
-                    width: geo.size.width / 2,
-                    height: geo.size.height
-                )
-                
-                //  النص الأيمن (Shelf)
-                ZStack {
-                    
-                    // الخلفية
-                    Image("bkshlf")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(
-                            width: geo.size.width / 2,
-                            height: geo.size.height
-                        )
-                        .clipped()
-                    
-                    ShelfColumnView(
-                        xPosition: viewModel.currentShelfIndex == 0 ? 366 : 658
-                    )
-                    
-                    // الأسهم
-                    HStack {
-                        Button {
-                            viewModel.goPrevious()
-                        } label: {
-                            Image(systemName: "chevron.left")
-                                .font(.largeTitle)
-                                .foregroundColor(.black)
-                        }
-                        .disabled(viewModel.currentShelfIndex == 0)
-                        
-                        Spacer()
-                        
-                        Button {
-                            viewModel.goNext()
-                        } label: {
-                            Image(systemName: "chevron.right")
-                                .font(.largeTitle)
-                                .foregroundColor(.black)
-                        }
-                        .disabled(viewModel.currentShelfIndex == viewModel.totalShelves - 1)
+        ZStack {
+            // الخلفية
+            Image("shelfbox")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+
+            GeometryReader { geo in
+                HStack(spacing: 0) {
+                    // LEFT HALF
+                    ZStack {
+                        Image("inbox")
+                            .resizable()
+                            .frame(width: 673, height: 529)
                     }
-                    .padding(.horizontal, 24)
+                    .frame(width: geo.size.width / 2, height: geo.size.height)
+                    .position(x: geo.size.width / 3.80, y: geo.size.height / 2)
+
+                    // RIGHT HALF
+                    VStack {
+                        ZStack {
+                            // رفوف
+                            shelf(x: 366, y: 140)
+                            shelf(x: 366, y: 365)
+                            shelf(x: 366, y: 589)
+                            shelf(x: 366, y: 814)
+                        }
+                        Spacer() // يدفع الأزرار للأسفل
+
+                        // أزرار أسفل النص
+                        HStack(spacing: 40) {
+                            Button(action: {
+                                print("Left arrow tapped")
+                            }) {
+                                Image(systemName: "chevron.left")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 70, height: 70)
+                                    .foregroundColor(.black)
+                                    .padding()
+                                    .background(Color(hexString: "FBDC7E"))
+                                    .clipShape(Circle())
+                            }
+
+                            Button(action: {
+                                print("Right arrow tapped")
+                            }) {
+                                Image(systemName: "chevron.right")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 70, height: 70)
+                                    .foregroundColor(.black)
+                                    .padding()
+                                    .background(Color(hexString: "FBDC7E"))
+                                    .clipShape(Circle())
+                            }
+                        }
+                        .padding(.bottom, 40) // مسافة من أسفل الشاشة
+                    }
+                    .frame(width: geo.size.width / 2, height: geo.size.height)
                 }
-                .frame(
-                    width: geo.size.width / 2,
-                    height: geo.size.height
-                )
             }
-            .ignoresSafeArea() // يغطي كامل الشاشة
         }
+    }
+
+    // MARK: - Shelf Component
+    func shelf(x: CGFloat, y: CGFloat) -> some View {
+        Image("shelf1")
+            .resizable()
+            .frame(width: 572, height: 78)
+            .position(x: x, y: y)
     }
 }
 
-
+// MARK: - HEX Color Support
+extension Color {
+    init(hexString: String) {
+        let scanner = Scanner(string: hexString)
+        scanner.currentIndex = hexString.startIndex
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        let r = Double((rgb >> 16) & 0xFF) / 255
+        let g = Double((rgb >> 8) & 0xFF) / 255
+        let b = Double(rgb & 0xFF) / 255
+        self.init(red: r, green: g, blue: b)
+    }
+}
 
 #Preview {
     ShelfBoxView()
